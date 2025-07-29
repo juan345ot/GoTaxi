f2785c3eea53b161da92178acfb7562efe8f2053
Juan Ignacio Scapellato <juanignacio295@hotmail.com>
2025-07-29 15:11:49 -0300

    App.js: solo AuthProvider, quita HistoryProvider, preparado para backend real

--- GoTaxiPassengerApp/App.js
+++ GoTaxiPassengerApp/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation/RootNavigator';
 import { AuthProvider } from './src/contexts/AuthContext';
-import { HistoryProvider } from './src/contexts/HistoryContext';
+import { LanguageProvider } from './src/contexts/LanguageContext';
 
 export default function App() {
   return (
     <AuthProvider>
-      <HistoryProvider>
+      <LanguageProvider>
         <NavigationContainer>
           <RootNavigator />
         </NavigationContainer>
-      </HistoryProvider>
+      </LanguageProvider>
     </AuthProvider>
   );
 }
